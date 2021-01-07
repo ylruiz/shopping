c625f82dfcc76c3612975bea089b73931089f40a
Yunet Luis Ruiz <yunet.lruiz94@gmail.com>
2021-01-07 18:12:17 +0100

    Use singleton +add  getList & getItems methods

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 
-import './models/list_items.dart';
-import './models/shoping_list.dart';
-import './util/dbhelper.dart';
+import 'models/list_items.dart';
+import 'models/shopping_list.dart';
+import 'util/dbhelper.dart';
+import 'ui/items_screen.dart';
 
 void main() => runApp(MyApp());
 
@@ -31,23 +32,39 @@ class ShList extends StatefulWidget {
 
 class _ShListState extends State<ShList> {
   DbHelper helper = DbHelper();
+  List<ShoppingList> shoppingList;
 
   @override
   Widget build(BuildContext context) {
-    showData();
-    return Container();
+    return ListView.builder(
+        itemCount: (shoppingList != null) ? shoppingList.length : 0,
+        itemBuilder: (BuildContext context, int index) {
+          return ListTile(
+            title: Text(shoppingList[index].name),
+            leading: CircleAvatar(
+              child: Text(shoppingList[index].priority.toString()),
+            ),
+            trailing: IconButton(
+              icon: Icon(Icons.edit),
+              onPressed: () {},
+            ),
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => ItemsScreen(shoppingList[index]),
+                )
+              );
+            },
+          );
+        });
   }
 
   Future showData() async {
     await helper.openDb();
-
-    ShoppingList list = ShoppingList(0, 'Bakery', 2);
-    int listId = await helper.insertList(list);
-
-    ListItem item = ListItem(0, listId, 'Bread', '1 kg', 'note');
-    int itemId = await helper.insertItem(item);
-
-    print('List Id: ${listId.toString()}');
-    print('Item Id: ${itemId.toString()}');
+    shoppingList = await helper.getList();
+    setState(() {
+      shoppingList = shoppingList;
+    });
   }
 }
