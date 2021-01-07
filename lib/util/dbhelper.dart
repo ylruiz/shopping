bf73ff85ab297cab4f3806880482e535534862ee
Yunet Luis Ruiz <yunet.lruiz94@gmail.com>
2021-01-07 15:40:05 +0100

    Adding insertList() & insertItem()

--- lib/util/dbhelper.dart
+++ lib/util/dbhelper.dart
@@ -1,26 +1,29 @@
 import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
 
+import '../models/list_items.dart';
+import '../models/shoping_list.dart';
+
 class DbHelper {
   final int version = 1;
   Database db;
 
-  Future<Database> openDB() async {
+  Future<Database> openDb() async {
     if (db == null) {
       db = await openDatabase(join(await getDatabasesPath(), 'shopping.db'),
           onCreate: (database, version) {
         database.execute(
             'CREATE TABLE lists(id INTEGER PRIMARY KEY, name TEXT, priority INTEGER)');
         database.execute(
-            'CREATE TABLE items(id INTEGER PRIMARY KEY), idList INTEGER, name TEXT, quantity TEXT, note TEXT ' +
-                'FOREIGN KEY(idList) PREFERENCES lists(id)');
+            'CREATE TABLE items(id INTEGER PRIMARY KEY, idList INTEGER, name TEXT, quantity TEXT, note TEXT, ' +
+                'FOREIGN KEY(idList) REFERENCES lists(id))');
       }, version: version);
     }
     return db;
   }
 
   Future testDb() async {
-    db = await openDB();
+    db = await openDb();
     await db.execute('INSERT INTO lists VALUES (0, "Fruits", 2)');
     await db.execute(
         'INSERT INTO items VALUES (0, 0, "Apples", "2 kg", "Better if they are grenn")');
@@ -28,5 +31,23 @@ class DbHelper {
     List items = await db.rawQuery('select * from items');
     print(lists[0].toString());
     print(items[0].toString());
+  }
+
+  Future<int> insertList(ShoppingList list) async {
+    int id = await this.db.insert(
+          'lists',
+          list.toMap(),
+          conflictAlgorithm: ConflictAlgorithm.replace,
+        );
+    return id;
+  }
+
+  Future<int> insertItem(ListItem item) async {
+    int id = await db.insert(
+      'items',
+      item.toMap(),
+      conflictAlgorithm: ConflictAlgorithm.replace,
+    );
+    return id;
   }
 }
