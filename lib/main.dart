4131206e62545206b1b5335499149c3f649bba8c
Yunet Luis Ruiz <yunet.lruiz94@gmail.com>
2021-01-07 15:40:25 +0100

    Cretating ShList

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import './models/list_items.dart';
+import './models/shoping_list.dart';
 import './util/dbhelper.dart';
 
 void main() => runApp(MyApp());
@@ -7,15 +9,45 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    DbHelper helper = DbHelper();
-    helper.testDb();
-
     return MaterialApp(
       title: 'Shopping List',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Container(),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Shopping List'),
+        ),
+        body: ShList(),
+      ),
     );
+  }
+}
+
+class ShList extends StatefulWidget {
+  @override
+  _ShListState createState() => _ShListState();
+}
+
+class _ShListState extends State<ShList> {
+  DbHelper helper = DbHelper();
+
+  @override
+  Widget build(BuildContext context) {
+    showData();
+    return Container();
+  }
+
+  Future showData() async {
+    await helper.openDb();
+
+    ShoppingList list = ShoppingList(0, 'Bakery', 2);
+    int listId = await helper.insertList(list);
+
+    ListItem item = ListItem(0, listId, 'Bread', '1 kg', 'note');
+    int itemId = await helper.insertItem(item);
+
+    print('List Id: ${listId.toString()}');
+    print('Item Id: ${itemId.toString()}');
   }
 }
